3cc667001adb733d96347c399d42679f3cd8ed54
Sergey Pestov <pestov-sa@yandex.ru>
2020-03-14 17:01:14 +0300

    game-display

--- src/index.js
+++ src/index.js
@@ -1,19 +1,51 @@
 
+const defaultSize = {
+  width: 30,
+  height: 30,
+};
+
+const display = new ROT.Display({
+  width: defaultSize.width,
+  height: defaultSize.height,
+  fontSize: 16,
+  forceSquareRatio: true,
+});
+
 const store = new Vuex.Store({
   state: {
     screen: 'mainmenu',
+    defaultSize,
   },
   mutations: {
     startNewGame(state) {
       state.screen = 'game';
+    },
+    openMainMenu(state) {
+      state.screen = 'mainmenu';
+    },
+  }
+});
+
+Vue.component('game-display', {
+  template: '#game-display',
+  methods: {
+    draw() {
+      display.draw(1, 1, '@', 'white', 'black');
     }
+  },
+  mounted() {
+    this.$el.appendChild(display.getContainer());
+    this.draw();
+  },
+  updated() {
+    this.draw();
   }
 });
 
 new Vue({
   store,
   methods: {
-    ...Vuex.mapMutations(['startNewGame']),
+    ...Vuex.mapMutations(['startNewGame', 'openMainMenu']),
   },
   computed: {
     ...Vuex.mapState(['screen']),
